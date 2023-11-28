0b090731a19b6c8ed8fa196a461ae2ae73826e94
Jenil Patel <48551188+Jenil245@users.noreply.github.com>
2023-11-28 22:26:30 +0000

    testing main

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        print(f"error : {e}")
