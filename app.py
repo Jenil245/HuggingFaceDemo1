d6123c5c37381b1ad561f2d5ef0103745679ef21
Jenil Patel <48551188+Jenil245@users.noreply.github.com>
2023-11-28 22:55:01 +0000

    testing main

--- app.py
+++ app.py
@@ -9,7 +9,3 @@ def index():
         return render_template('index.html')
     except Exception as e:
         print(f"error : {e}")
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
