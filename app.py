5d6f4932d6ef40c0b8307fa5308cccbd4d57bbeb
Jenil Patel <48551188+Jenil245@users.noreply.github.com>
2023-11-28 20:42:54 +0000

    changes

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 from flask import Flask, render_template, request
-from transformers import pipeline
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
 app = Flask(__name__)
 
 # Load the ASR model from Hugging Face
-asr_model = pipeline(task="automatic-speech-recognition", model="facebook/whisper-large-v3")
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "openai/whisper-large-v3"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    max_new_tokens=128,
+    chunk_length_s=30,
+    batch_size=16,
+    return_timestamps=True,
+    torch_dtype=torch_dtype,
+    device=device,
+)
 
 @app.route('/')
 def index():
@@ -22,7 +46,7 @@ def transcribe():
 
     try:
         # Perform ASR using the Hugging Face model
-        transcription = asr_model(audio_file.read())[0]['sentence']
+        transcription = pipe(audio_file.read())[0]['sentence']
 
         return render_template('result.html', transcription=transcription)
 
